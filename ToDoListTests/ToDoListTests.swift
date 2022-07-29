5ed1696589e308e206ac130132bebe5019a4058e
Kamil Khairullin <kamil.khayrullin.personal@yandex.ru>
2022-07-30 02:59:00 +0300

    Создан пустой проект
    - Минимальная версия - IOS 11
    - Удален storyboard
    - Удален SceneDelegate
    - Добавлен SwiftLint в Build Phases перед компиляцией

---
+++ ToDoListTests/ToDoListTests.swift
@@ -0,0 +1,14 @@
+@testable import ToDoList
+import XCTest
+
+class ToDoListTests: XCTestCase {
+    override func setUpWithError() throws {}
+
+    override func tearDownWithError() throws {}
+
+    func testExample() throws {}
+
+    func testPerformanceExample() throws {
+        self.measure {}
+    }
+}
