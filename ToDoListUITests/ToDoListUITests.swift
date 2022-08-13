876ea43f625dd85d753d1399653a9261b9cfa507
Kamil Khairullin <kamil.khayrullin.personal@yandex.ru>
2022-08-13 19:46:38 +0300

    removed performance test

--- ToDoListUITests/ToDoListUITests.swift
+++ ToDoListUITests/ToDoListUITests.swift
@@ -12,11 +12,11 @@ class ToDoListUITests: XCTestCase {
         app.launch()
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+//    func testLaunchPerformance() throws {
+//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+//            measure(metrics: [XCTApplicationLaunchMetric()]) {
+//                XCUIApplication().launch()
+//            }
+//        }
+//    }
 }
